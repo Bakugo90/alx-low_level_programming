4a780bd08a22b881f860d0c634a869fba2d1167d
Samadou ouro agorouko <souroagorouko@gmail.com>
2022-07-02 15:20:02 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main-Entry level
+ *
+ * Description: print with a new line
+ * Return: always reurn 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
