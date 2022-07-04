43a82bc8bb20e26aa72b326b1f36c4a9831f658b
Samadou ouro agorouko <souroagorouko@gmail.com>
2022-07-04 16:54:38 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry-point
+ * 
+ * Description: print the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+	{
+	if (letter != 'e' && letter != 'q')
+	putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
