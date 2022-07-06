4a6c9aee34449dfa2d5f0baf98a20fe7ef56c043
Samadou ouro agorouko <souroagorouko@gmail.com>
2022-07-06 12:42:38 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: variable text
+ * Descriotion: Variable text
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
