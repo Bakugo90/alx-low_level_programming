3e04099789b479ade2349993a7e43971c14eb5e6
Samadou ouro agorouko <souroagorouko@gmail.com>
2022-07-06 15:49:35 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+nclude "main.h"
+/**
+ * print_sign - prints signs of a number
+ * @n: takes integer paramer n
+ * Return: 1 if the number is positive
+ * 0 if the number is zero
+ * 1 if the number is negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+
