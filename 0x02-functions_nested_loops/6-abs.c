eb495b41f07cc5ed9ac71f26e0e874ca686482d0
Samadou ouro agorouko <souroagorouko@gmail.com>
2022-07-06 16:10:19 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.c"
+
+/**
+ *  * _abs - computes the absolute value of a number
+ *   * @c: takes c as a parameter
+ *
+ *    * Return: 0
+ */
+int _abs(int c)
+{
+	if (c >= 0)
+		return (c);
+	else
+		return (-c);
+}
