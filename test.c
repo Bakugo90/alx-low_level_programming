a528b438dc29e875bfbb9f67172740cb1b39f39e
SamadouOuroagorouko <Souroagorouko@gmail.com>
2022-08-13 10:45:07 +0000

    singly linked list

--- test.c
+++ test.c
@@ -5,32 +5,10 @@
  * user- Learning c structure
  */
 
-struct User
+struct structure
 {
-    char *name;
-    char *email;
-    int age;
-};
+    int a;
+    struct structure *suivant;
+}
 
-
-struct User  *nouvelle_utilisateur(char *nom, char *prenom, int age)
-{
-    struct User user;
-    user = malloc(sizeof(struct User));
-};
-
-
- int main(void)
- {
-    struct User user;
-
-    user.name =  "Samadou";
-    user.email = "Souroagorouko@gmail.com";
-    user.age = 22;
-
-    printf("The user %s has connected\n", user.name);
-    printf("With this email %s\n", user.email);
-    printf("His age is %d\n", user.age);
-
-    return (0);
- }
+struct structure *head = NULL;
