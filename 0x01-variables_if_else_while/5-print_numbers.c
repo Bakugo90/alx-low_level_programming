9d49463a313cbb6a0278f32135c3247adabe21db
SamadouOuroagorouko <Souroagorouko@gmail.com>
2022-08-15 22:47:57 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main: Entry point
+ *
+ * Description: print single digit number
+ * return: Always return 0 (on success)
+ */
+
+int main(void)
+{
+    int number;
+    number = '0';
+
+        while (number <= '9')
+        {
+            putchar(number);
+            number++;
+        }
+    putchar('\n');
+}
