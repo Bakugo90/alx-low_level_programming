76a1c88b4dc206d9791d784eed6adc0a3538232f
SamadouOuroagorouko <Souroagorouko@gmail.com>
2022-08-15 22:26:18 +0000

    fixing betty warnings

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,14 +9,14 @@
 
 int main(void)
 {
-	char letter;
+char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+for (letter = 'a'; letter <= 'z'; letter++)
 
 	{
 	if (letter != 'e' && letter != 'q')
 	putchar(letter);
 	}
-	putchar('\n');
-	return (0);
+putchar('\n');
+return (0);
 }
