6122486b3f8f37536cdf5c96f71a96cee53416eb
SamadouOuroagorouko <Souroagorouko@gmail.com>
2022-10-03 18:40:47 +0000

    print digit from 0 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main-Entry point
+ * @void: ...
+ *
+ * Return: digit number from 0 to 9.
+ */
+
+int main(void)
+{
+int i;
+for ( i = 0; i < 10; i++ )
+{
+putchar(i);
+}
+return (0);
+}
