f71a5af88db4fb139c2e575ce7eed6b6a2f73171
Samadou Ouro-agorouko <souroagorouko@gmail.com>
2024-03-09 07:35:36 +0000

    intel assembly & stderr printing

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main-Entry level
+ *
+ * Description: print at stderr with a new line
+ * Return: always return 1
+ */
+int main(void)
+{
+        fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 62, 1, stderr);
+                return (1);
+}
